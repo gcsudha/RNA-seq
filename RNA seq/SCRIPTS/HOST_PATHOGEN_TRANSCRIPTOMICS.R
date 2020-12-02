03be5835acb0e45e2318c33a31145fd0c5b09cb4
David Wheeler <davidwheeler@wless-user-172019182162.temp.wsu.edu>
2020-12-02 10:27:27 -0800

    data,figure,tables,scripts

---
+++ RNA seq/SCRIPTS/HOST_PATHOGEN_TRANSCRIPTOMICS.R
@@ -0,0 +1,100 @@
+#-----------------------------------------------------------------------#
+# THE TRANSCRIPTOMES OF SYMPTOMATIC HOSTS, POTATO AND MINT, AND         #
+# ASYMPTOMATIC HOST, MUSTARD, DURING INFECTION WITH HOST-ADAPTED STRAINS#
+# OF VERTICILLIUM DAHLIAE.                                              #
+# DAVID LINNARD WHEELER                                                 #
+# 8/27/2017                                                             #
+#-----------------------------------------------------------------------#
+
+# METADATA OUTLINE:
+# OBJECTIVES
+#  TEST Ho HYPOTHESES  
+# EXPERIMENTAL DESIGN
+# OPEN AND CURATE DATA
+# SUMMARY STATISTICS
+# EXPLORATORY DATA ANALYSES
+# DIAGNOSTICS
+# PARAMETRIC ANALYSES
+# BAYESIAN ANALYSES
+
+#-----------------------------------------------------------------------#
+# OBJECTIVES
+# TEST HYPOTHESES
+# SCIENCE Ho 1: DIFFERENTIALLY EXPRESSED GENE PROFILES ARE THE 
+  # SAME ACROSS SYMPTOMATIC AND ASYMPTOMATIC HOSTS
+# SCIENCE Ho 2: SYMPTOMATIC HOSTS DO NOT RESPOND TO V.DAHIAE INFECTION 
+  # WITH DEG PROFILES
+# SCIENCE Ho 3: STRAINS OF V. DAHLIAE DO NOT EXHIBIT DEGS PROFILES 
+  # BETWEEN SYMPTOMATIC HOSTS OR BETWEEN SYMPTOMATIC vs. ASYMPTOMATIC HOSTS
+# STATISTICAL Ho 1-3: 
+  # Observed variation in DEG across treatments represents
+  # random variation, not systematic effects of hosts or isolates
+  # Variation in the DEG is unrelated to variation in the hosts and isolates
+  # and is no greater than expected by chance or sampling error.
+
+#-----------------------------------------------------------------------#
+# EXPERIMENTAL DESIGN 1 
+# TREATMENT STRUCTURE: 3 WAY FACTORIAL DESIGN
+# INDEPENDENT VARIABLES: 
+  # 3 HOSTS (POTATO, MINT, MUSTARD) x 
+      # 1 POTATO
+      # 2 MINT
+      # 3 MUSTARD
+  # 3 ISOLATES (653, 111, CONTROLS) in planta  x
+      # 1 653
+      # 2 111
+      # 3 CONTROL
+  # 2 TIME POINTS (10 dpi and at symptom expression) x
+      # 1 10 dpi
+      # 2 SYMPTOM EXPRESSION
+  # 10 REPLICATES x
+  # 1 TRIALS = 90 EXPERIMENTAL UNITS
+    # 1.	Potato x 3 Isolates x 2 time points x 10 replicates = 60 experimental units
+      #a.	Potato: non-inoculated control @ T1 and T2
+      #b.	Potato inoculated with 653 @ T1 and T2
+      #c.	Potato inoculated with 111 @ T1 and T2
+    #2.	Mint x 3 Isolates x 2 time points x 10 replicates = 60 experimental units 
+      #a.	Mint: non-inoculated control @ T1 and T2
+      #b.	Mint inoculated with 111 @ T1 and T2
+      #c.	Mint inoculated with 653 @ T1 and T2
+    #3.	Mustard x 3 Isolates x 2 time points x 10 replicates = 60 experimental units
+      #a.	Mustard: non-inoculated control @ T1 and T2
+      #b.	Mustard  inoculated with 653 @ T1 and T2
+      #c.	Mustard inoculated with 111@ T1 and T2
+# DEPENDENT VARIABLE: RNA TRANSCRIPT SEQUENCES AND COUNTS
+# DESIGN STRUCTURE: RANDOMIZED COMPLETE BLOCK DESIGN
+# EXPERIMENTAL UNIT: PLANT
+# OBSERVATIONAL UNIT: PLANT
+# SAMPLES: 3 BIOLOGIAL PLANT STEMS ?
+# SUBSAMPLES: PLANT STEM SECTIONS
+# DATA: RNA TRANSCRIPT SEQUENCES AND COUNTS 
+# ANALYSES: 
+
+# QUESIONS:
+
+#-----------------------------------------------------------------------#
+# MATERIALS AND METHODS
+# INOCULUM PREPARATION FOR ROOT DIP (3.5"pot):
+  # INOCULUM FOR 111:
+    # 180 plants/3 isolates = 60 plants/isolate * 100 ml/plant (Dung et al. 2010) = 6000 ml = 6 L inoculum
+    # 6 L of 10^6 conidia/ml inoculum is needed
+    # 6 L/200 ml/flask = 30 flasks 
+  # INOCULUM FOR 653:
+    # 180 plants/3 isolates = 60 plants/isolate * 100 ml/plant (Dung et al. 2010) = 6000 ml = 6 L inoculum
+    # 6 L of 10^6 conidia/ml inoculum is needed
+    # 6 L/200 ml/flask = 30 flasks 
+    
+# TRIAL ONE PLANTED: 5/1/2018
+# TRIAL ONE INOCULATED: 5/19/2018
+# FIRST HARVEST: POTATO, MINT AND MUSTARD HARVESTED @ 2 DPI: 5/21/2018
+# SECOND HARVEST: MUSTARD: 5/29/2018
+
+#-----------------------------------------------------------------------#
+#-----------------------------------------------------------------------#
+# OPEN AND CURATE DATA
+# SUMMARY STATISTICS
+# EXPLORATORY DATA ANALYSES
+# DIAGNOSTICS
+# PARAMETRIC ANALYSES
+# MANUSCRIPT
+#-----------------------------------------------------------------------#
